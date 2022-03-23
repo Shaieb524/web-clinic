fc29cb4a627be8c35cc0c1df4ebf210ca82bc119
Joud <joud.shaieb@medicus.ai>
2022-03-23 14:32:07 +0200

    add schedule generator

---
+++ helpers/schedule_helpers.go
@@ -0,0 +1,35 @@
+package helpers
+
+import (
+	"github.com/Shaieb524/web-clinic.git/models"
+)
+
+// create specified No. of appointments item for a specified docter
+func GenerateAppointementSlots(slotsNo int, doctorId string) []models.AppointmentSlot {
+	apptpointmentSlots := make([]models.AppointmentSlot, slotsNo)
+
+	for i := range apptpointmentSlots {
+		apptpointmentSlots[i].DoctorId = doctorId
+	}
+
+	return apptpointmentSlots
+}
+
+// create a weelky schedule for a specified doctor
+func GenerateWeekDoctorSchedule(doctorId string) models.DoctorSchedule {
+	weekDays := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"}
+	schedule := make(map[string]models.ScheduleDay)
+	appointmentSlots := GenerateAppointementSlots(3, doctorId)
+
+	for i := range weekDays {
+		sd := models.ScheduleDay{}
+		sd.Appointments = appointmentSlots
+		schedule[weekDays[i]] = sd
+	}
+
+	var ds models.DoctorSchedule
+	ds.DoctorId = doctorId
+	ds.Schedule = schedule
+
+	return ds
+}
